3d26c8ef57475911dcf4b7b3ea27e5d6ecc44884
KoryunY <yeritsyankoryun@gmail.com>
2024-06-04 15:23:03 -0700

    asdasda

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const { TonConnectUI, TonConnect } = require('@tonconnect/ui');
 const express = require('express');
 const path = require('path');
 
@@ -13,20 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Handle POST request for connecting to the wallet
-app.get('/connect-wallet', async (req, res) => {
-  try {
-    res.json({msg:"scs"});
-  //   const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-  //     manifestUrl: '/public/tonconnect-manifest.json',
-  //     buttonRootId: 'ton-connect'
-  // });
-  //   const connectedWallet = await tonConnectUI.connectWallet();
-    res.json(connectedWallet);
-  } catch (error) {
-    console.error('Error connecting to wallet:', error);
-    res.status(500).json({ error: 'Error connecting to wallet' });
-  }
-});
+// app.get('/connect-wallet', async (req, res) => {
+//   try {
+//     //res.json({msg:"scs"});
+//     const tonConnectUI = new TonConnect().connect({
+//       manifestUrl: '/public/tonconnect-manifest.json',
+//       buttonRootId: 'ton-connect'
+//   });
+//     const connectedWallet = await tonConnectUI.connectWallet();
+//     res.json(connectedWallet);
+//   } catch (error) {
+//     console.error('Error connecting to wallet:', error);
+//     res.status(500).json({ error: 'Error connecting to wallet' });
+//   }
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
